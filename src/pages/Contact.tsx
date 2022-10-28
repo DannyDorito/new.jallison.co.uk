cc446de523f3a34d987e56229f977f18e67d810d
John Allison <john@jallison.co.uk>
2022-10-28 22:11:38 +0100

    cleanup css

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -4,12 +4,14 @@ import '../scss/Contact.scss';
 const Contact = () => {
   return (
     <>
-      <h1 className='no-select'>Contact me!</h1>
-      {socialData.map((social) => (
-        <a href={social.link} key={social.platform}>
-          {social.platform}
-        </a>
-      ))}
+      <div>
+        <h1 className='no-select'>Contact me!</h1>
+        {socialData.map((social) => (
+          <a href={social.link} key={social.platform}>
+            {social.platform}
+          </a>
+        ))}
+      </div>
     </>
   );
 };
