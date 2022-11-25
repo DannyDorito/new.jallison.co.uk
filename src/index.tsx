05482faf1d806197af23f741b7049d8d887f072e
John Allison <john@jallison.co.uk>
2022-11-25 19:04:46 +0000

    apollo errors now console.error

--- src/index.tsx
+++ src/index.tsx
@@ -20,12 +20,12 @@ const httpLink = new HttpLink({
 const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors) {
     graphQLErrors.forEach(({ message, locations, path }) =>
-      console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
+      console.error(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
     );
   }
 
   if (networkError) {
-    console.log(`[Network error]: ${networkError}`);
+    console.error(`[Network error]: ${networkError}`);
   }
 });
 
