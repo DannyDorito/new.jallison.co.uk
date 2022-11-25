40ebf425feab816965b6c2f640e5a0a6682fba04
John Allison <john@jallison.co.uk>
2022-11-25 23:15:17 +0000

    Create RouterSetup.test.tsx

---
+++ src/tests/pages/RouterSetup.test.tsx
@@ -0,0 +1,33 @@
+import { render, screen } from "@testing-library/react";
+import { MockedProvider } from "@apollo/client/testing";
+import RouterSetup from "../../pages/RouterSetup";
+
+describe("When the page is initially loaded", () => {
+  it("should render <RouterSetup /> JSX component", async () => {
+    render(
+      <MockedProvider mocks={[]} addTypename={false}>
+        <RouterSetup />
+      </MockedProvider>
+    );
+
+    expect(
+      await screen.findByText("Home")
+    ).toBeInTheDocument();
+
+    expect(
+      await screen.findByText("Contact")
+    ).toBeInTheDocument();
+
+    expect(
+      await screen.findByText("Projects")
+    ).toBeInTheDocument();
+
+    expect(
+      await screen.findByText("Resume")
+    ).toBeInTheDocument();
+
+    expect(
+      await screen.findByText("John Allison")
+    ).toBeInTheDocument();
+  });
+});
