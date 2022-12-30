7212a4f4ac4f920d1472696d3004356cfa43969b
John Allison <john@jallison.co.uk>
2022-12-30 19:26:17 +0000

    Update Education.test.tsx

--- src/tests/jest/components/Education.test.tsx
+++ src/tests/jest/components/Education.test.tsx
@@ -1,11 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import Education from "../../../components/Education";
+import { education } from "../../../types/EducationData";
 
 describe( "When the page is rendered", () =>
 {
   it( "should render the <Education /> JSX component", async () =>
   {
+    const expectedEducation = education;
     render(
       <MemoryRouter>
         <Education />
@@ -15,5 +17,28 @@ describe( "When the page is rendered", () =>
     expect(
       await screen.findByText( "Education" )
     ).toBeInTheDocument();
+
+    expectedEducation.forEach( async expected =>
+    {
+      expect(
+        await screen.findByText( expected.institution )
+      ).toBeInTheDocument();
+
+      expect(
+        await screen.findByText( expected.qualification )
+      ).toBeInTheDocument();
+
+      expect(
+        await screen.findByText( expected.result )
+      ).toBeInTheDocument();
+
+      expect(
+        await screen.findByText( expected.location )
+      ).toBeInTheDocument();
+
+      expect(
+        await screen.findByText( `${ expected.startDate.toLocaleString( 'default', { month: 'long' } ) } ${ expected.startDate.getFullYear() } - ${ expected.endDate.toLocaleString( 'default', { month: 'long' } ) } ${ expected.endDate.getFullYear() }` )
+      ).toBeInTheDocument();
+    } );
   } );
 } );
