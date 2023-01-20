c9e77d64bdfc8d5e44f9ee311a2f8f1f620fdc35
John Allison <john@jallison.co.uk>
2023-01-20 20:08:21 +0000

    Update About.test.tsx

--- src/tests/pages/About.test.tsx
+++ src/tests/pages/About.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react"
 import { MemoryRouter } from "react-router-dom";
 import About from "../../pages/About";
 
-describe( " When the page is rendered", () =>
+describe( "When the page is rendered", () =>
 {
   it( "should render <About /> JSX component", async () =>
   {
