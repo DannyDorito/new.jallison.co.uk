240326aa375609dfbdb4f3fd9a3011902cc4dab3
John Allison <john@jallison.co.uk>
2023-02-06 19:17:26 +0000

    backend fixes and package updates

--- src/pages/Guestbook.tsx
+++ src/pages/Guestbook.tsx
@@ -1,6 +1,6 @@
-import { FormEvent, useEffect, useState } from 'react';
-import '../scss/pages/Guestbook.scss';
-import { GuestbookEntry } from '../types/Guestbook';
+import { FormEvent, useEffect, useState } from "react";
+import "../scss/pages/Guestbook.scss";
+import { GuestbookEntry } from "../types/Guestbook";
 
 const Guestbook = () =>
 {
@@ -11,69 +11,86 @@ const Guestbook = () =>
   {
     const getComments = async () =>
     {
-      const response = await fetch( `${process.env.REACT_APP_GUESTBOOK_API_ENDPOINT}getall/take=10&skip=0`, {
-        method: "GET"
-      } );
-      await response.json()
+      const response = await fetch(
+        `${ process.env.REACT_APP_GUESTBOOK_API_ENDPOINT }getall&take=10&skip=0`,
+        {
+          method: "GET",
+        }
+      );
+      await response
+        .json()
         .then( ( guestbookEntries: string[] ) =>
         {
           guestbookEntries.forEach( ( guestbookEntry ) =>
           {
-            const guestbookEntryObj = Object.assign( new GuestbookEntry( undefined, undefined ), guestbookEntry ) as GuestbookEntry;
-            setComments( comments => [ ...comments, guestbookEntryObj ] )
-          } )
-        } ).catch( ( r ) =>
+            const guestbookEntryObj = Object.assign(
+              new GuestbookEntry( undefined, undefined ),
+              guestbookEntry
+            ) as GuestbookEntry;
+            setComments( ( comments ) => [ ...comments, guestbookEntryObj ] );
+          } );
+        } )
+        .catch( ( r ) =>
         {
           console.log( r );
         } );
-    }
+    };
 
     getComments();
   }, [] );
-
 
   const handleComment = async ( event: FormEvent<HTMLFormElement> ) =>
   {
     event.preventDefault();
     try
     {
-      let response = await fetch( `${process.env.REACT_APP_GUESTBOOK_API_ENDPOINT}post`, {
-        method: "POST",
-        headers: { 'Content-type': 'application/json' },
-        body: JSON.stringify( new GuestbookEntry(
-          "hello",
-          "john",
-        ) )
-      } );
+      let response = await fetch(
+        `${ process.env.REACT_APP_GUESTBOOK_API_ENDPOINT }post`,
+        {
+          method: "POST",
+          headers: { "Content-type": "application/json" },
+          body: JSON.stringify( new GuestbookEntry( "hello", "john" ) ),
+        }
+      );
       if ( response.ok )
       {
-        setComments( comments => [ ...comments, new GuestbookEntry( comment, "john" ) ] );
+        setComments( ( comments ) => [
+          ...comments,
+          new GuestbookEntry( comment, "john" ),
+        ] );
       }
     } catch ( error )
     {
       console.log( error );
     }
-  }
+  };
 
   const updateComment = ( c: string ) =>
   {
     setComment( c );
-  }
+  };
 
   return (
     <>
       <div>
-        <h1 className='no-select'>Guestbook</h1>
-        <h2 className='no-select'>Leave a comment below!</h2>
-        {comments.map( ( guestBookEntry ) => <p key={guestBookEntry.id}>{guestBookEntry.message}</p> )}
+        <h1 className="no-select">Guestbook</h1>
+        <h2 className="no-select">Leave a comment below!</h2>
+        {comments.map( ( guestBookEntry ) => (
+          <p key={guestBookEntry.id}>
+            {guestBookEntry.message} by: {guestBookEntry.createdBy}
+          </p>
+        ) )}
         <form onSubmit={handleComment}>
           <label></label>
-          <input type="text" onChange={( event ) => updateComment( event.target.value )}></input>
+          <input
+            type="text"
+            onChange={( event ) => updateComment( event.target.value )}
+          ></input>
           <input type="submit" />
         </form>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Guestbook;
